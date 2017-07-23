743147ee917f1b2ef94469052f0c8779bcaf55c5
Anthony Damico <ajdamico@gmail.com>
2017-07-23 02:09:00 -0400

    sampling to keep build within restrictions

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-lodown( "nhanes" , output_dir = file.path( getwd() ) )
+
+nhanes_cat <-
+	get_catalog( "nhanes" ,
+		output_dir = file.path( getwd() ) )
+
+# sample 50% of the records
+which_records <- sample( seq( nrow( nhanes_cat ) ) , round( nrow( nhanes_cat ) * 0.50 ) )
+
+# always sample years == "2013-2014"
+nhanes_cat <- unique( nhanes_cat[ which_records , ] , subset( nhanes_cat , years == "2013-2014" ) )
+
+lodown( "nhanes" , nhanes_cat )
