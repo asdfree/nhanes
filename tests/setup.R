ac6c2e15619fe9b889047d17e4ccfd7b34e2ad41
Anthony Damico <ajdamico@gmail.com>
2017-07-23 02:32:45 -0400

    rbind obviously

--- tests/setup.R
+++ tests/setup.R
@@ -10,6 +10,6 @@ nhanes_cat <-
 which_records <- sample( seq( nrow( nhanes_cat ) ) , round( nrow( nhanes_cat ) * 0.50 ) )
 
 # always sample years == "2013-2014"
-nhanes_cat <- unique( nhanes_cat[ which_records , ] , subset( nhanes_cat , years == "2013-2014" ) )
+nhanes_cat <- unique( rbind( nhanes_cat[ which_records , ] , subset( nhanes_cat , years == "2013-2014" ) ) )
 
 lodown( "nhanes" , nhanes_cat )
