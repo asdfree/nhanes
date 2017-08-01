b2e07bd9147557e8f2f574473631c0bcb236dcd4
Anthony Damico <ajdamico@gmail.com>
2017-08-01 07:28:37 -0400

    25% of nhanes

--- tests/setup.R
+++ tests/setup.R
@@ -6,8 +6,8 @@ nhanes_cat <-
 	get_catalog( "nhanes" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 50% of the records
-which_records <- sample( seq( nrow( nhanes_cat ) ) , round( nrow( nhanes_cat ) * 0.50 ) )
+# sample 25% of the records
+which_records <- sample( seq( nrow( nhanes_cat ) ) , round( nrow( nhanes_cat ) * 0.25 ) )
 
 # always sample years == "2013-2014"
 nhanes_cat <- unique( rbind( nhanes_cat[ which_records , ] , subset( nhanes_cat , years == "2013-2014" ) ) )
